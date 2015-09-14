780f387471b31fc20fcf3d97e6dfbb9e7929c9fb
Matthew <mkendon@gmail.com>
2015-09-14 22:46:10 +0100

    added new child theme

---
+++ web/app/themes/shindig-child/functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Matthew Kendon <matt@outlandish.com>
+ */
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    $parent_style = 'parent-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        [$parent_style]
+    );
+}
